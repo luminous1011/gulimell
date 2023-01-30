ccc9f620cd8ad28bf602cd1bae13ed225ed80e41
panjinshan <pjspan@foxmail.com>
2023-01-30 15:34:43 +0800

    init

---
+++ gulimall-member/src/test/java/com/guigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigu.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
